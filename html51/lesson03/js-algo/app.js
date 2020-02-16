b52267d3811e5252faa18d49868d02849496bc19
JuliiaTrach <60792683+JuliiaTrach@users.noreply.github.com>
2020-02-16 17:49:24 +0200

    Lesson01, 03

---
+++ html51/lesson03/js-algo/app.js
@@ -0,0 +1,22 @@
+function writeAlgoDemo1() {
+    var side_a = Math.random() * 10 + 5;
+    var side_b = Math.random() * 10 + 5;
+    document.write("<p>Side a=" + side_a + "</p><p>Side b=" + side_b + "</p>");
+    document.write("<p>Объем куба" + side_a * side_a * side_a + "</p>");
+    document.write("<p>Площадь квадрата" + side_a * side_a + "</p>");
+    document.write("<p>Площадь прямоугольника" + side_a * side_b + "</p>");
+}
+
+function writeAlgoDemo2() {
+    var side_a = Math.random() * 10 + 5;
+    var side_b = Math.random() * 10 + 5;
+    document.write("<p>Side a=" + side_a + "</p><p>Side b=" + side_b + "</p>");
+    document.write("<p>Площадь прямоугольного треугольника" + side_a * side_b / 2 + "</p>");
+}
+
+function writeAlgoDemo3() {
+    var radius = Math.random() * 10 + 5;
+    var square = Math.PI * radius * radius;
+    document.write("<p>Radius= " + radius + "</p>");
+    document.write("<p>Площадь круга" + square + "</p>");
+}
