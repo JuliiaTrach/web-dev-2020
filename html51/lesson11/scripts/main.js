6013b0960c766985bb198917447c83ff954ec2f1
JuliiaTrach <60792683+JuliiaTrach@users.noreply.github.com>
2020-02-21 21:17:42 +0200

    lesson11

---
+++ html51/lesson11/scripts/main.js
@@ -0,0 +1,12 @@
+function startLogic() {
+
+    var newHtml = '<em>New Header</em>';
+
+    document.getElementById('headerItem').innerHTML = newHtml;
+
+}
+startLogic();
+
+function clickHandler() {
+    alert('You clicked me!');
+}
